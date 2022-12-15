afa8f638fae5c5693095a5d59e1c6dcf15dca45e
sumit kamble <sumitbkamble2@gmail.con>
2022-12-15 18:05:28 +0530

    openAI backend added

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotnev = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// enable body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/openai', require('./routes/openaiRoutes'));
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
