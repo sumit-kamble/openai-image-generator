cc1f163bbf86fc8de99a2cd1ca5482237633ff7e
sumit kamble <sumitbkamble2@gmail.con>
2022-12-17 09:19:41 +0530

    frontend added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotnev = require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -7,6 +8,10 @@ const app = express();
 // enable body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/openai', require('./routes/openaiRoutes'));
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
